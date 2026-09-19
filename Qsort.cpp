bd61f08baafcc07f20f4a9996f97b65b85d9f9f0
tikuznezov <timkuz08@gmail.com>
2026-09-20 01:52:01 +0300

    Упростил алгоритм, ближе к стандартному

--- Qsort.cpp
+++ Qsort.cpp
@@ -2,94 +2,88 @@
 
 void   Swap(void *a, void *b, size_t element_size)
 {
+    // printf("Start SWAP\n");
     void *temp = calloc(1, element_size + 1);
     memcpy(temp, a, element_size);
     memcpy(a   , b, element_size);
     memcpy(b   , temp, element_size);
+    free(temp);
+    // printf("End SWAP\n");
 }
 
 size_t Part (void *array, size_t array_length, size_t element_size, int (* Comp)(const void *a, const void *b))
 {
-    assert(array);
     if (array_length <= 1)
-        return NULL;
+        return 0;
+    assert(array);
 
-
-    size_t base_index = (size_t)rand() % array_length;                                                           // crate random index in array
+    size_t base_index = (size_t)rand() % array_length;                                                                   // crate random index in array
     void *base = memcpy(calloc(1, element_size), (const void*)((char *)array + element_size*base_index), element_size);  // initialization of base element
 
     size_t l = 0;
     size_t r = array_length-1;
+    // printf("Start PART: len = %zu;   l = %zu, r = %zu\n",array_length, l, r);
 
-    while ((l < r) && (l < array_length) && (l >= 0) &&  (r < array_length) && (r >= 0))
+    while (l <= r)
     {
-        if (!Comp((const void*)((char *)array + l*element_size), (const void*)base))
-        {
+        while ((Comp((const void*)((char *)array + l*element_size), (const void*)base) < 0) && (l < array_length))
             l++;
-        }
-        else
+        while ((Comp((const void*)((char *)array + r*element_size), (const void*)base) > 0) && (r > 0))
+            r--;
+        if (l >= array_length || r == 0) break;
+        if (l <= r)
         {
-            while ((Comp((const void*)((char *)array + r*element_size), base)) && (r >= 0) && (r < array_length) && (l < r))
-            {
-                r--;
-            }
-
             Swap((void *)((char *)array + r*element_size), (void *)((char *)array + l*element_size), element_size);
+            l++;
+            r--;
         }
     }
-
-    if ((l == 0) && Comp((const void*)((char *)array + l*element_size), base))
-    {
-        for (size_t i = 1; i < array_length; i++)
-            if (Comp((const void*)((char *)array + l*element_size), base))
-                return l;
-        return (size_t)-1;
-    }
-
+    // printf("End PART\n");
+    if (l >= array_length)
+        l = array_length-1;
     free(base);
     return l;
 }
 
+
+/// @brief 
+/// @param array 
+/// @param array_length 
+/// @param element_size 
+/// @param Comp 
 void   Qsort(void *array, size_t array_length, size_t element_size, int (* Comp)(const void *a, const void *b))
 {
     assert(array);
 
-    if (array_length == 1)
+    if (array_length <= 1)
         return;
 
     else if (array_length == 2)
     {
-        if (Comp((const void*)((char *)array + 0*element_size), (const void*)((char *)array + 1*element_size)))
+        if (Comp((const void*)((char *)array + 0*element_size), (const void*)((char *)array + 1*element_size)) > 0)
             Swap((void*)((char *)array + 0*element_size), (void*)((char *)array + 1*element_size), element_size);
         return;
     }
 
     else if (array_length == 3)
     {
-        if (Comp((const void*)((char *)array + 0*element_size), (const void*)((char *)array + 1*element_size)))
+        if (Comp((const void*)((char *)array + 0*element_size), (const void*)((char *)array + 1*element_size)) > 0)
             Swap((void*)((char *)array + 0*element_size), (void*)((char *)array + 1*element_size), element_size);
 
-        if (Comp((const void*)((char *)array + 1*element_size), (const void*)((char *)array + 2*element_size)))
+        if (Comp((const void*)((char *)array + 1*element_size), (const void*)((char *)array + 2*element_size)) > 0)
             Swap((void*)((char *)array + 1*element_size), (void*)((char *)array + 2*element_size), element_size);
 
-        if (Comp((const void*)((char *)array + 0*element_size), (const void*)((char *)array + 1*element_size)))
+        if (Comp((const void*)((char *)array + 0*element_size), (const void*)((char *)array + 1*element_size)) > 0)
             Swap((void*)((char *)array + 0*element_size), (void*)((char *)array + 1*element_size), element_size);
         return;
     }
-    else if (array_length > 0)
+    else
     {
-        size_t right_pointer = Part(array, array_length, element_size, Comp);
+        // printf("Start SORT\n");
+        size_t Rstart = Part(array, array_length, element_size, Comp);
 
-        if (right_pointer == (size_t)-1)
-            return;
-
-        else if (right_pointer > 0)
-        {
-            Qsort(array, right_pointer, element_size, Comp);
-        }
-
-        size_t Rlen = array_length - right_pointer;
-        void  *Raddress = (void *)((char *)array + element_size * right_pointer);
-        Qsort(array, Rlen, element_size, Comp);; // Сам опорный элемент гарантированно стоит на нужном месте, так что его пропускаем
+        Qsort((char *)array + Rstart*element_size, array_length - Rstart, element_size, Comp);
+        Qsort(array                              ,                Rstart, element_size, Comp);
+        // printf("End Sort\n");
     }
 }
